cd74b793c76a33c9a5872e3a8415068f6b6a725b
Dud1985 <d.curvino@gmail.com>
2026-02-25 10:09:20 +0100

    updated index file

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,11 +1,13 @@
 import { en } from './en';
 import { de } from './de';
 import { ru } from './ru';
+import { it } from './it';
 
 export const locales = {
   en,
   de,
   ru,
+  it,
 };
 
 export type SupportedLanguage = keyof typeof locales;
